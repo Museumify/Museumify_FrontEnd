100ff10794adb7656a6c3ec8f41a4ce14a90c74c
Reneh7 <renehm2001@gmail.com>
2023-07-24 15:31:29 +0300

    reneh-darkmode-done

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import NavBarList from './components/navCompnent/NavBarList';
 import FavoriteArt from './components/favoriteArt/FavoriteArt';
+import { createContext, useState } from 'react';
+
+export const ThemeContext= createContext(null);
+
 function App() {
+  const [theme,setTheme]=useState("light")
+  const toggleTheme=() =>{
+    setTheme((curr)=> (curr ==="light"? "dark":"light"))
+  }
   return (
-    <div className="App">
-      <NavBarList/>
-     <Routes>
+    <ThemeContext.Provider value={{theme,toggleTheme}}>
+    <div className="App" id={theme}>
+       <NavBarList/> 
+      <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/favorite" element={<FavoriteArt/>} />
-      </Routes>
-      
+      </Routes> 
     </div>
+    </ThemeContext.Provider>
   );
 }
 
