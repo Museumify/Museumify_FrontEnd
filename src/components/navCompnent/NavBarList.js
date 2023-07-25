d50b0aa0f379ed2f9f2320f20c6452e32857867e
Reneh7 <renehm2001@gmail.com>
2023-07-25 20:21:07 +0300

    auth-signout-profile-done

--- src/components/navCompnent/NavBarList.js
+++ src/components/navCompnent/NavBarList.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBarList.css';
-import { ThemeContext } from  "../../App";  
+import { ThemeContext } from '../../App';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar'; 
+import Navbar from 'react-bootstrap/Navbar';
 import ReactSwitch from 'react-switch';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ProfileModal from '../screens/Profile';
+import LogoutButton from '../Logout';
+import { useAuth0 } from '@auth0/auth0-react';
 
+function NavBarList() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false); 
+  const { user } = useAuth0(); 
 
-function NavBarList(){
-    return(
-      <ThemeContext.Consumer>
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleToggleModal = () => {
+    setShowModal((prevState) => !prevState);
+  };
+
+  return (
+    <ThemeContext.Consumer>
       {({ theme, toggleTheme }) => (
         <div>
           <Navbar bg="dark" data-bs-theme="dark">
@@ -20,13 +35,26 @@ function NavBarList(){
                 <Nav.Link href="/favorite">Favorite Art</Nav.Link>
               </Nav>
               <div className="switch">
-                <ReactSwitch
-                  onChange={toggleTheme}
-                  checked={theme === 'dark'}
-                />
+                <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'} />
+              </div>
+              <div className="user-info">
+                <Dropdown show={menuOpen} alignRight>
+                  <Dropdown.Toggle variant="link" id="user-dropdown-toggle" onClick={handleMenuToggle}>
+                    {user?.picture && <img src={user.picture} alt={user.name} className="navbar-user-image" />}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item>{user?.name}</Dropdown.Item>
+                    <Dropdown.Item onClick={handleToggleModal}>Your Profile</Dropdown.Item>
+                    <Dropdown.Item>
+                      <LogoutButton />
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
               </div>
             </Container>
           </Navbar>
+          <ProfileModal showModal={showModal} handleToggleModal={handleToggleModal} />
         </div>
       )}
     </ThemeContext.Consumer>
@@ -34,3 +62,10 @@ function NavBarList(){
 }
 
 export default NavBarList;
+
+
+
+
+
+
+
