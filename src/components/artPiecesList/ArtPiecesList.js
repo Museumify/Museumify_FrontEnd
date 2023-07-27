9d411b2654b5be9fe36e0fb87f92875b3e2d135c
MDarwish993 <m.darwish993@hotmail.com>
2023-07-27 08:32:32 +0000

    MohammadStylingV01

--- src/components/artPiecesList/ArtPiecesList.js
+++ src/components/artPiecesList/ArtPiecesList.js
@@ -6,8 +6,8 @@ function ArtPiecesList(props) {
     return (
         <div className="main"
             style={{
-                display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '100px',
-                justifyContent: 'center',marginLeft:'50px',marginTop:'200px'
+                display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '20px',
+                justifyContent: 'center',marginTop:'50px'
             }}
         >
             {props.data.slice(0, 104).map((obj, i) => {
@@ -17,6 +17,7 @@ function ArtPiecesList(props) {
                     && obj.id !== 113202 && obj.id !== 98627 && obj.id !== 145463 && obj.id !== 144743
                     && obj.id !== 169187 && obj.id !== 141644) {
                     return <ArtPiece key={i} data={obj} commentHandler={props.commentHandler} />;
+                 
                 } else {
                     return null;
                 }
@@ -30,3 +31,8 @@ function ArtPiecesList(props) {
 }
 
 export default ArtPiecesList;
+
+
+// return (<div style={{transform: `rotate(${(Math.random() - 0.5) * 10}deg)`}} >
+// <ArtPiece key={i} data={obj} commentHandler={props.commentHandler} />;
+// </div>)
